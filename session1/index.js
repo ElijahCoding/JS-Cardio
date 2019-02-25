30d6f5b7eef9080e424e342b4958b4e85b37a3f3
ElijahCoding <cuiyeqing960904@gmail.com>
2019-02-25 22:26:12 +0300

    maxCharacter

--- session1/index.js
+++ session1/index.js
@@ -99,7 +99,28 @@ function capitalizeLetters(str) {
 // CHALLENGE 5: MAX CHARACTER
 // Return the character that is most common in a string
 // ex. maxCharacter('javascript') == 'a'
-function maxCharacter(str) {}
+function maxCharacter(str) {
+    let charMap = {}
+    let maxNum = 0
+    let maxChar = ''
+
+    str.split('').forEach(char => {
+        if (charMap[char]) {
+            charMap[char]++
+        } else {
+            charMap[char] = 1
+        }
+    })
+
+    for (let char in charMap) {
+        if (charMap[char] > maxNum) {
+            maxNum = charMap[char]
+            maxChar = char
+        }
+    }
+
+    return maxChar
+}
 
 
 
@@ -110,6 +131,6 @@ function fizzBuzz() {}
 
 
 // Call Function
-const output = capitalizeLetters('i love js');
+const output = maxCharacter('javascript');
 
 console.log(output);
