6adf1a695d70cca6ccaef8eb86e81d8b05149ed8
ElijahCoding <cuiyeqing960904@gmail.com>
2019-02-27 09:16:24 +0300

    session 2 finished

--- session2/index.js
+++ session2/index.js
@@ -94,11 +94,19 @@ function formatStr(str) {
 // ex. 'hello there' === 'Ifmmp UIfsf'
 
 function letterChanges(str) {
-    let newStr = str.replace(/[a-z]/gi, (char) => {
+    let newStr = str.toLowerCase().replace(/[a-z]/gi, (char) => {
         if (char === 'z' || char === 'Z') {
-            
+            return 'a'
+        } else {
+            return String.fromCharCode(char.charCodeAt() + 1)
         }
     })
+
+    newStr = newStr.replace(/a|e|i|o|u/gi, (vowel) => {
+        return vowel.toUpperCase()
+    })
+
+    console.log(newStr);
 }
 
 // Call Function
